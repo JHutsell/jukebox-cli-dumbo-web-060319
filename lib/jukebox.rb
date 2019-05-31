a0223e86a91910a3e8c782c1ad4be5c77bafc471
Justin Hutsell <hutselljustin@gmail.com>
2019-05-31 15:37:01 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -28,7 +28,8 @@ def play(songs)
   puts "Please enter a song name or number:"
   song_choice = gets.chomp
   songs.each_with_index do |song, idx|
-    if song_choice
+    if song_choice.to_i == idx 
+      puts 
   end
   
 end
