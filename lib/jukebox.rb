9b08e4307ef9a0615e0f7196ba8bf9ee366dd83a
Justin Hutsell <hutselljustin@gmail.com>
2019-05-31 15:53:29 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -31,7 +31,7 @@ def play(songs)
     if song_choice.to_i == i 
       puts "playing #{songs[i - 1]}"
     elsif song_choice == song
-      puts "playing #{songs[i]}"
+      puts "playing #{song}"
     else
       puts "Invalid input, please try again"
     end
